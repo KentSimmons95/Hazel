38ddcc5f6afcc71f4810c9d3401104a26e0a78e8
Kent Simmons <kent1372@gmail.com>
2022-08-02 21:02:12 +1200

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,22 @@
+#include "Application.h"
+
+namespace Hazel
+{
+
+	Hazel::Application::Application()
+	{
+	}
+
+	Hazel::Application::~Application()
+	{
+	}
+
+	void Hazel::Application::Run()
+	{
+		while (true)
+		{
+
+		}
+	}
+
+}
